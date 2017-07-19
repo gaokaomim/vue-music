3e1963971d516f8b70ce2dc75d27910d822b3784
gaokaomim <gaokaomim@163.com>
2017-07-19 16:58:09 +0800

    完成项目骨架

--- src/main.js
+++ src/main.js
@@ -1,11 +1,14 @@
+import 'babel-polyfill'
 import Vue from 'vue'
 import App from './App'
-// import router from './router'
+import router from './router'
 import './common/stylus/index.styl'
-Vue.config.productionTip = false
-
+import fastclick from 'fastclick'
 /* eslint-disable no-new */
+
+fastclick.attach(document.body)
 new Vue({
   el: '#app',
-  render: h => h(App)
+  render: h => h(App),
+  router
 })
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,26 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Hello from '@/components/Hello'
-
+import Recommend from 'components/recommend/recommend'
+import Singer from 'components/singer/singer'
+import Search from 'components/search/search'
+import Rank from 'components/rank/rank'
 Vue.use(Router)
 
 export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'Hello',
-      component: Hello
-    }
-  ]
+  routes: [{
+    path: '/',
+    redirect: '/recommend'
+  }, {
+    path: '/recommend',
+    component: Recommend
+  }, {
+    path: '/singer',
+    component: Singer
+  }, {
+    path: '/search',
+    component: Search
+  }, {
+    path: '/rank',
+    component: Rank
+  }]
 })
