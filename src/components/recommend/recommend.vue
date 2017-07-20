2a1a9a39e4834dcfb201343272669c8fe29b7a63
gaokaomim <gaokaomim@163.com>
2017-07-20 17:52:42 +0800

    获取到轮播图的数据并且完成轮播图的效果

--- src/components/recommend/recommend.vue
+++ src/components/recommend/recommend.vue
@@ -1,31 +1,48 @@
 <template>
-    <div class="recommend">
-        <div class="recommend-content">
-            <div class="slider-wrapper"></div>
-            <div class="recommend-list">
-                <h1 class="list-title">热门歌单推荐</h1>
-                <ul></ul>
-            </div>
-        </div>
+  <div class="recommend">
+    <div class="recommend-content">
+      <div v-if="recommends.length" class="slider-wrapper">
+        <slider>
+          <div v-for="item in recommends">
+            <a :href="item.linkUrl">
+              <img :src="item.picUrl">
+            </a>
+          </div>
+        </slider>
+      </div>
+      <div class="recommend-list">
+        <h1 class="list-title">热门歌单推荐</h1>
+        <ul></ul>
+      </div>
     </div>
+  </div>
 </template>
 
 <script type="text/ecmascript-6">
-  import { getRecommend } from 'api/recommend'
-  import { ERR_OK } from 'api/config'
-  export default {
-    created() {
-      this._getRecommend()
-    },
-    methods: {
-      _getRecommend() {
-        getRecommend().then((res) => {
-          if (res.code === ERR_OK) {
-            console.log(res.data.slider)
-          }
-        })
-      }
+import Slider from 'base/slider/slider'
+import { getRecommend } from 'api/recommend'
+import { ERR_OK } from 'api/config'
+export default {
+  data() {
+    return {
+      recommends: []
     }
+  },
+  created() {
+    this._getRecommend()
+  },
+  methods: {
+    _getRecommend() {
+      getRecommend().then((res) => {
+        if (res.code === ERR_OK) {
+          this.recommends = res.data.slider
+        }
+      })
+    }
+  },
+  components: {
+    Slider
+  }
 }
 </script>
 
