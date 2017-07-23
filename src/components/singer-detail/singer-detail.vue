ea32ab0e85372b25a370a9740d2def38b6379641
gaokaomim <gaokaomim#163.com>
2017-07-23 15:31:35 +0800

    新增vuex

--- src/components/singer-detail/singer-detail.vue
+++ src/components/singer-detail/singer-detail.vue
@@ -1,11 +1,22 @@
 <template>
-    <transition name="slide">
-        <div class="singer-detail">
-        </div>
-    </transition>
+  <transition name="slide">
+    <div class="singer-detail">
+    </div>
+  </transition>
 </template>
 
-<script type="text/ecmascript-6">
+ <script type="text/ecmascript-6">
+import { mapGetters } from 'vuex'
+export default {
+  computed: {
+    ...mapGetters([
+      'singer'
+    ])
+  },
+  created() {
+    console.log(this.singer)
+  }
+}
 </script>
 <style scoped lang="stylus" rel="stylesheet/stylus">
   @import "~common/stylus/variable"
--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import 'babel-polyfill'
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import store from './store'
 import './common/stylus/index.styl'
 import fastclick from 'fastclick'
 import VueLazyLoad from 'vue-lazyload'
@@ -9,11 +10,12 @@ import VueLazyLoad from 'vue-lazyload'
 
 fastclick.attach(document.body)
 Vue.use(VueLazyLoad, {
-  loading: require('common/image/default.png')
+    loading: require('common/image/default.png')
 })
 new Vue({
-  el: '#app',
-  render: h => h(App),
-  router
+    el: '#app',
+    render: h => h(App),
+    router,
+    store
 
 })
